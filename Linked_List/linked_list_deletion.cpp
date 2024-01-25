ff77925c435ca76764f6e79f72481eebd10a155c
Nazmul Hasan <nazmulhasancsecu@gmail.com>
2024-01-26 01:38:29 +0600

    function to delete n'th node, first node that contain the data==n

---
+++ Linked_List/linked_list_deletion.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+
+    Node(int data){
+        this->data=data;
+        this->next=nullptr;
+    }
+};
+
+void deleteAtPosition(Node** head, int position){
+    if(head==nullptr){
+        cout<<"Error: List is empty!"<<endl;
+        return;
+    }
+
+    if(position==0){
+        *head=(*head)->next;
+        return;
+    }
+
+    Node* current=*head;
+    int nodeCount=0;
+    while(nodeCount<position-1 && current->next!=nullptr){
+        current=current->next;
+        nodeCount++;
+    }
+    if(current->next==nullptr){
+        cout<<"Given \"Position\" is out of bound"<<endl;
+        return;
+    }
+    Node* nodeToDelete=current->next;
+    current->next=nodeToDelete->next;
+}
+
+void deleteNodeWithValue(Node** head, int value){
+    if(head==nullptr){
+        cout<<"Error: List is empty!"<<endl;
+        return;
+    }
+    
+    Node* current=*head;
+    while(current->next->data!=value && current->next!=nullptr)
+        current=current->next;
+
+    if(current->next==nullptr){
+        cout<<"Given \"Value\" doesn't match with any data in the list"<<endl;
+        return;
+    }
+    Node* nodeToDelete=current->next;
+    current->next=nodeToDelete->next;
+}
